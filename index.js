d7d5b593b321c06006b603a5ee635c95f364c982
babaTheCoder <131319600+babaTheCoder@users.noreply.github.com>
2023-12-02 21:16:41 +0000

    completed rock paper scissors game

---
+++ index.js
@@ -0,0 +1,47 @@
+let computerChoiceArr = ["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);
+    computerSelection = computerChoiceArr[randomChoice];
+    computerSelection = computerSelection.toLocaleLowerCase();
+
+    return computerSelection;
+}
+
+function getPlayerChoice () {
+    playerSelect = prompt("Enter a choice [rock, paper, scissors]: ");
+    playerSelect = playerSelect.toLocaleLowerCase();
+    return playerSelect;
+}
+
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        return `It's a tie`
+    }
+
+    if (playerSelection === "paper" && computerSelection === "rock"){
+        return `You win! Paper beats Rock`
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return `You lose! Paper beats Rock`
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win! Scissors beat Paper`
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return `You lose! Scissors beat Paper`
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `You win! Rock beats Scissors`
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return `You lose! Rock beats Scissors`
+    } 
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let result = playRound(getPlayerChoice(), getComputerChoice());
+        console.log(result);
+    }
+}
+
+game();
+
